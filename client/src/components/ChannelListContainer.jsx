95960c5497a28f4c03766ded9559994d99a65a11
Neel Ratan Guria <neelratan@live.com>
2022-06-27 12:36:00 +0530

    sidebar

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,9 +3,10 @@ import { ChannelList, useChatContext } from 'stream-chat-react'
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './'
 import Cookies from 'universal-cookie'
 import HospitalIcon from '../assets/hospital.png'
+import LogOutIcon from '../assets/logout.png'
 
 
-const sideBar = () => {
+const SideBar = () => {
     return (
         <div className='channel-list__sidebar'>
             <div className='channel-list__sidebar__icon1'>
@@ -15,17 +16,41 @@ const sideBar = () => {
             </div>
             <div className='channel-list__sidebar__icon2'>
                 <div className='icon2__inner'>
-                    <img src={HospitalIcon} alt='logo' width="30" />
+                    <img src={LogOutIcon} alt='logout' width="30" />
                 </div>
             </div>
         </div>
     )
 }
 
-const ChannelListContainer = () => {
-  return (
-    <div>{sideBar()}</div>
-  )
-}
+const CompanyHeader = () => (
+    <div className='channel-list__header'>
+        <p className='channel-list__header__text'>
+            Medical Pager
+        </p>
+    </div>
+)
+
+const ChannelListContainer = () => (
+    <>
+        <SideBar />
+        <div className='channel-list__list__wrapper'>
+            <CompanyHeader />
+            <ChannelSearch />
+            <ChannelList
+                filters={{}}
+                channelRenderFilterFn={() => { }}
+                List={(listProps) => <TeamChannelList
+                    type="team"
+                />
+                }
+                Preview={(previewProps) => <TeamChannelPreview {...previewProps}
+                    type="team"
+                />}
+            />
+        </div>
+
+    </>
+)
 
 export default ChannelListContainer
